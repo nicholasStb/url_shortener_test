b41f6d96643b4df8adb6bf1e462ce6c2431969ed
nicholas <168528835+nicholasStb@users.noreply.github.com>
2024-08-17 03:29:48 +0800

    Update to show metadata title

--- src/app/layouts/RootLayout.tsx
+++ src/app/layouts/RootLayout.tsx
@@ -15,6 +15,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
+            <head>
+                <title>{metadata.title}</title>
+                <meta name="description" content={metadata.description} />
+            </head>
             <body className={inter.className}>
                 {children}
             </body>
